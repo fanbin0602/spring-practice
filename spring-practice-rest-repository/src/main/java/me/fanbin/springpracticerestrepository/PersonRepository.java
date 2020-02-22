bbdde22a95639516cc63128980ec50a2bcb4a741
Fan Bin <fanbin0602@163.com>
2020-02-22 23:18:05 +0800

    添加 REST 访问 JPA 数据示例

---
+++ spring-practice-rest-repository/src/main/java/me/fanbin/springpracticerestrepository/PersonRepository.java
@@ -0,0 +1,18 @@
+package me.fanbin.springpracticerestrepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * @author fanbin
+ * @date 2020/2/22
+ */
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
---
+++ spring-practice-rest-repository/src/main/java/me/fanbin/springpracticerestrepository/SpringPracticeRestRepositoryApplication.java
@@ -0,0 +1,20 @@
+package me.fanbin.springpracticerestrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 示例：REST 接口访问 JPA Data
+ * @see <a href="https://spring.io/guides/gs/accessing-data-rest/">
+ *     Accessing JPA Data with REST</a>
+ *
+ * @author fanbin
+ */
+@SpringBootApplication
+public class SpringPracticeRestRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeRestRepositoryApplication.class, args);
+    }
+
+}
